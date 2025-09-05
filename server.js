459db029da190be4f72fff1482c711873a951405
BilalUnknown10 <bilal9342610@gmail.com>
2025-09-05 15:41:43 +0500

    fix db contion file name

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const userRoutes = require('./Routes/User_routes');
 const adminRoutes = require('./Routes/Admin_routes')
-const dataBaseConnection = require('./db_Connection/conn');
+const dataBaseConnection = require('./DB_Connection/conn');
 const PORT = process.env.PORT;
 
 
