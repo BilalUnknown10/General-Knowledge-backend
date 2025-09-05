1742aeac3a0a7309387760106338eef6801ce5a3
BilalUnknown10 <bilal9342610@gmail.com>
2025-09-05 16:03:21 +0500

    mongodb uri change to string

--- DB_Connection/conn.js
+++ DB_Connection/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dataBaseConnection = async () => {
     try {
-        const DB_connection = mongoose.connect(process.env.MONGODB_URI);
+        const DB_connection = mongoose.connect(`${process.env.MONGODB_URI}`);
         console.log("database connected successfully")
     } catch (error) {
         console.log("error in database connection : ", error);
