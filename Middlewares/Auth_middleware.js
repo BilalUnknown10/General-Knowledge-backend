d5d3010cf385d45867384d6151b3572c87068994
BilalUnknown10 <bilal9342610@gmail.com>
2025-09-06 01:30:47 +0500

    middleware change for undefined token

--- Middlewares/Auth_middleware.js
+++ Middlewares/Auth_middleware.js
@@ -5,7 +5,7 @@ const authMiddleware = async (req, res, next) => {
 try {
   const token = req.headers?.authorization.replace("Bearer ", "").trim();
 
-if (!token) {
+if (!token || token === undefined) {
   return res.status(401).json({ message: "No token" });
 }
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
